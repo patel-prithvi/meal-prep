4dee681f8f4306f24bcd7e0ff660c8d8d3a1b338
Prithvi Patel <prithvitpatel@gmail.com>
2026-06-13 13:10:03 +0530

    render deployment setup

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        dbname="Meal_prep",
-        user="postgres",
-        password="Prithvi@20",
-        host="localhost",
-        port=5433
+        os.getenv("DATABASE_URL"),
+        sslmode="require"
     )
